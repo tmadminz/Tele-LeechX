b0406969d11045c1666d07940368c89a11539625
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2021-08-29 12:18:42 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -51,7 +51,7 @@ class Progress:
             [
                 [
                     InlineKeyboardButton(
-                        "Cancel 🚫",
+                        "⛔ Cancel ⛔",
                         callback_data=(
                             f"gUPcancel/{chat_id}/{mes_id}/{from_user}"
                         ).encode("UTF-8"),
@@ -82,7 +82,7 @@ class Progress:
                 ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
                 round(percentage, 2))
             #cpu = "{psutil.cpu_percent()}%"
-            tmp = progress + "\n**• Total 📀:**`〘{1}〙`\n**• Done ✓ :**` 〘{0}〙`\n**• Speed 🚀 :** `〘{2}〙`\n**• ETA ⏳ :**` 〘{3}〙`".format(
+            tmp = progress + "\n**⌧ Total 🗃:**`〚{1}〛`\n**⌧ Done ✅ :**`{0}`\n**⌧ Speed 📊 :** `{2}`\n**⌧ ETA 🔃 :**`{3}`".format(
                 humanbytes(current),
                 humanbytes(total),
                 humanbytes(speed),
