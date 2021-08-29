be073a8543f3776274c682ebc985732a03cd7e61
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2021-08-29 12:42:07 +0530

    ♻️ RECYCLE ♻️

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -77,7 +77,7 @@ class Progress:
             elapsed_time = TimeFormatter(milliseconds=elapsed_time)
             estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-            progress = "\n<code>{0}{1} {2}%</code>\n".format(
+            progress = "\n<code>[{0}{1}] {2}%</code>\n".format(
                 ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
                 ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
                 round(percentage, 2))
