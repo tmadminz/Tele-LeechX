134c327e033d5e3f53768404ca394d6164484b67
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2021-09-09 16:29:14 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -43,7 +43,8 @@ from tobrot import (
     RCLONE_COMMAND,
     TOGGLE_DOC,
     HELP_COMMAND,
-    SPEEDTEST
+    SPEEDTEST,
+    TSEARCH_COMMAND
 )
 from tobrot.helper_funcs.download import down_load_media_f
 from tobrot.plugins.call_back_button_handler import button
@@ -262,7 +263,7 @@ if __name__ == "__main__":
     ##############################################################################
     get_speed_handler = MessageHandler(
         get_speed,
-        filters=filters.command([f"{SPEEDTEST}"])
+        filters=filters.command([f"{SPEEDTEST}", f"{SPEEDTEST}@{bot.username}"])
         & filters.chat(chats=AUTH_CHANNEL),
     )
     app.add_handler(get_speed_handler)
