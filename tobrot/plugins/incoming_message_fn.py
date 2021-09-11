90b5df7ad3850d07bcda4f6d95b92f05bcd7d017
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2021-09-11 10:12:19 +0530

    Wrap Up #1

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -111,10 +111,11 @@ async def incoming_message_f(client, message):
         is_cloud = False
         is_unzip = False
         bot_unzip = f"{LEECH_UNZIP_COMMAND}@{bot.username}"
+        bot_unzip = f"{LEECH_ZIP_COMMAND}@{bot.username}"
 
         if user_command == LEECH_UNZIP_COMMAND.lower() or bot_unzip.lower():
             is_unzip = True
-        elif user_command == LEECH_ZIP_COMMAND.lower():
+        elif user_command == LEECH_ZIP_COMMAND.lower() or bot_zip.lower():
             is_zip = True
 
         if user_command == GLEECH_COMMAND.lower():
