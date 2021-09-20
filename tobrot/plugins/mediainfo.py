337fa921ae32536b7a496b5fbe5d43f93c59e137
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2021-09-20 14:34:12 +0530

    Done 🛠

--- tobrot/plugins/mediainfo.py
+++ tobrot/plugins/mediainfo.py
@@ -18,12 +18,12 @@ from tobrot.helper_funcs.bot_commands import BotCommands
 def post_to_telegraph(a_title: str, content: str) -> str:
     """ Create a Telegram Post using HTML Content """
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "slam-mirrorbot"
+    auth_name = "FuZionX-Leech"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=a_title,
         author=auth_name,
-        author_url="https://github.com/breakdowns/slam-mirrorbot",
+        author_url="https://github.com/5MysterySD/Tele-LeechX",
         text=content,
     )
     return post_page["url"]
