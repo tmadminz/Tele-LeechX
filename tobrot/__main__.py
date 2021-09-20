9ee486f2ce7ea7647e3d5508d4453aefddfe1feb
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2021-09-20 14:41:17 +0530

    Add to Main

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -44,7 +44,8 @@ from tobrot import (
     TOGGLE_DOC,
     HELP_COMMAND,
     SPEEDTEST,
-    TSEARCH_COMMAND
+    TSEARCH_COMMAND,
+    MEDIAINFO_CMD
 )
 from tobrot.helper_funcs.download import down_load_media_f
 from tobrot.plugins.call_back_button_handler import button
@@ -60,6 +61,7 @@ from tobrot.plugins.incoming_message_fn import (g_clonee, g_yt_playlist,
                                                 rename_tg_file)
 from tobrot.plugins.new_join_fn import help_message_f, new_join_f
 from tobrot.plugins.speedtest import get_speed
+from tobrot.plugins.mediainfo import mediainfo
 from tobrot.plugins.rclone_size import check_size_g, g_clearme
 from tobrot.plugins.status_message_fn import (
     cancel_message_f,
@@ -275,6 +277,13 @@ if __name__ == "__main__":
         & filters.chat(chats=AUTH_CHANNEL),
     )
     app.add_handler(searchhelp_handler)
+    ##############################################################################
+    mediainfo_handler = MessageHandler(
+        mediainfo,
+        filters=filters.command([f"{MEDIAINFO_CMD}", f"{MEDIAINFO_CMD}@{bot.username}"])
+        & filters.chat(chats=AUTH_CHANNEL),
+    )
+    app.add_handler(mediainfo_handler)
     ##############################################################################
 
 
