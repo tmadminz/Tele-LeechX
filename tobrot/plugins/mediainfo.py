7dc6bff835d44dc6dc10144f4526574598ccfbb9
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2021-09-21 09:34:00 +0530

    Change Dic
    Lets Check If My Knowledge Works

--- tobrot/plugins/mediainfo.py
+++ tobrot/plugins/mediainfo.py
@@ -78,7 +78,7 @@ async def mediainfo(client, message):
        return
     media_type = str(type(x_media)).split("'")[1]
     file_path = safe_filename(await reply.download())
-    output_ = await runcmd(f'mediainfo "{file_path}"')
+    output_ = await runcmd(f'/plugins/mediainfo "{file_path}"')
     out = None
     if len(output_) != 0:
          out = output_[0]
