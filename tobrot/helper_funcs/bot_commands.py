d59f79d6db4e878e5d96dbe22235c5865a536259
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2021-10-08 12:12:31 +0530

    Update bot_commands.py

--- tobrot/helper_funcs/bot_commands.py
+++ tobrot/helper_funcs/bot_commands.py
@@ -1,20 +1,38 @@
+from tobrot import (
+    LEECH_COMMAND,
+    LEECH_UNZIP_COMMAND,
+    LEECH_ZIP_COMMAND,
+    YTDL_COMMAND, 
+    STATUS_COMMAND,
+    SAVE_THUMBNAIL,
+    CLEAR_THUMBNAIL,
+    LOG_COMMAND,
+    RENAME_COMMAND,
+    TOGGLE_VID,
+    TOGGLE_DOC,
+    HELP_COMMAND,
+    SPEEDTEST,
+    TSEARCH_COMMAND,
+    BOT_NO
+    )
+
 class _BotCommands:
     def __init__(self):
-        self.LeechCommand = 'leech'
-        self.ExtractCommand = 'extract'
-        self.ArchiveCommand = 'archive'
-        self.ToggleDocCommand = 'toggledoc'
-        self.ToggleVidCommand = 'togglevid'
-        self.SaveCommand = 'savethumb'
-        self.ClearCommand = 'clearthumb'
-        self.RenameCommand = 'rename'
-        self.StatusCommand = 'status'
-        self.SpeedCommand = 'speedtest'
-        self.YtdlCommand = 'ytdl'
+        self.LeechCommand = f'{LEECH_COMMAND}'
+        self.ExtractCommand = f'{LEECH_UNZIP_COMMAND}'
+        self.ArchiveCommand = f'{LEECH_ZIP_COMMAND}'
+        self.ToggleDocCommand = f'{TOGGLE_DOC}'
+        self.ToggleVidCommand = f'{TOGGLE_VID}'
+        self.SaveCommand = f'{SAVE_THUMBNAIL}'
+        self.ClearCommand = f'{CLEAR_THUMBNAIL}'
+        self.RenameCommand = f'{RENAME_COMMAND}'
+        self.StatusCommand = f'{STATUS_COMMAND}'
+        self.SpeedCommand = f'{SPEEDTEST}'
+        self.YtdlCommand = f'{YTDL_COMMAND}'
         self.PytdlCommand = 'pytdl'
-        self.HelpCommand = 'help'
-        self.LogCommand = 'log'
+        self.HelpCommand = f'{HELP_COMMAND}'
+        self.LogCommand = f'{LOG_COMMAND}'
         self.MediaInfoCommand = 'mediainfo'
-        self.TsHelpCommand = 'tshelp'
+        self.TsHelpCommand = f'{TSEARCH_COMMAND}'
 
 BotCommands = _BotCommands()
