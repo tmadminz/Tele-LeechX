138d4707fca694f6bae7862d8cc9e999ac8064f3
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2021-10-08 11:21:31 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -21,7 +21,8 @@ from tobrot import (
     YTDL_COMMAND,
     GPYTDL_COMMAND,
     PYTDL_COMMAND,
-    BOT_NO
+    BOT_NO,
+    UPDATES_CHANNEL
 )
 from tobrot import bot
 from tobrot.helper_funcs.admin_check import AdminCheck
@@ -106,7 +107,7 @@ async def incoming_message_f(client, message):
             # LOGGER.info(aria_i_p)
         
         u_men = message.from_user.mention
-        await i_m_sefg.edit_text(f"┏━━━━━━━━━━━━━━━━╻\n┣👤 𝐔𝐬𝐞𝐫 : {u_men} \n┃\n┃ <code>⚡️ Your Request Has Been Added To The Status List ⚡️</code> \n┣ <b><u>Send <code>/status{BOT_NO}</code> To Check Your Progress</u></b>")
+        await i_m_sefg.edit_text(f"┏━━━━━━━━━━━━━━━━╻\n┣👤 𝐔𝐬𝐞𝐫 : {u_men} \n┃\n┃ <code>⚡️ Your Request Has Been Added To The Status List ⚡️</code> \n┣ <b><u>Send <code>/status{BOT_NO}</code> To Check Your Progress</u></b>\n┃\n┗━♦️ℙ𝕠𝕨𝕖𝕣𝕖𝕕 𝔹𝕪 UPDATES_CHANNEL♦️━╹")
         # try to download the "link"
         is_zip = False
         is_cloud = False
