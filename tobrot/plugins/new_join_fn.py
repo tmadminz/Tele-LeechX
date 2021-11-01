4fabf8a1a4d820a86c9b997d55f551d075c4a9b5
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2021-11-01 22:43:53 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -15,7 +15,7 @@ async def new_join_f(client, message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('Channel', url='https://t.me/MaxxBots')
+                        InlineKeyboardButton('Channel', url='https://t.me/FuZionXTorrentQuater')
                     ]
                 ]
                )
@@ -32,40 +32,12 @@ async def help_message_f(client, message):
     else:
         utxt = "Streamable"
     await message.reply_text(
-        f"""Available Commands
-/{RCLONE_COMMAND} : This will change your drive config on fly.(First one will be default)
- 
-/{CLONE_COMMAND_G}: This command is used to clone gdrive files or folder using gclone.
-Syntax:- `[ID of the file or folder][one space][name of your folder only(If the id is of file, don't put anything)]` and then reply /gclone to it.
- 
-/{LOG_COMMAND}: This will send you a txt file of the logs.
- 
-/{YTDL_COMMAND}: This command should be used as reply to a supported link
- 
-/{PYTDL_COMMAND}: This command will download videos from youtube playlist link and will upload to telegram.
- 
-/{GYTDL_COMMAND}: This will download and upload to your cloud.
- 
-/{GPYTDL_COMMAND}: This download youtube playlist and upload to your cloud.
- 
+        f"""𝗔𝘃𝗮𝗶𝗹𝗮𝗯𝗹𝗲 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀 : 
+  
 /{LEECH_COMMAND}: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [this command will SPAM the chat and send the downloads a seperate files, if there is more than one file, in the specified torrent]
  
 /{LEECH_ZIP_COMMAND}: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [This command will create a .tar.gz file of the output directory, and send the files in the chat, splited into PARTS of 1024MiB each, due to Telegram limitations]
- 
-/{GLEECH_COMMAND}: This command should be used as reply to a magnetic link, a torrent link, or a direct link. And this will download the files from the given link or torrent and will upload to the cloud using rclone.
- 
-/{GLEECH_ZIP_COMMAND} This command will compress the folder/file and will upload to your cloud.
- 
-/{LEECH_UNZIP_COMMAND}: This will unarchive file and upload to telegram.
- 
-/{GLEECH_UNZIP_COMMAND}: This will unarchive file and upload to cloud.
- 
-/{TELEGRAM_LEECH_COMMAND}: This will mirror the telegram files to ur respective cloud .
- 
-/{TELEGRAM_LEECH_UNZIP_COMMAND}: This will unarchive telegram file and upload to cloud.
- 
-/{GET_SIZE_G}: This will give you total size of your destination folder in cloud.
- 
+  
 /{RENEWME_COMMAND}: This will clear the remains of downloads which are not getting deleted after upload of the file or after /cancel command.
  
 /{CANCEL_COMMAND_G} [GID]: To cancel ur download
@@ -90,6 +62,12 @@ the file will be uploaded as new.txt.
 
 /{TOGGLE_DOC}: To Upload Your Files As Documents
 
+/{LOG_COMMAND}: This will send you a txt file of the logs.
+ 
+/{YTDL_COMMAND}: This command should be used as reply to a supported link
+ 
+/{PYTDL_COMMAND}: This command will download videos from youtube playlist link and will upload to telegram.
+
 **How to Use....?**
 __Send any one of the available command, as a reply to a valid link/magnet/torrent. 👊__
 
@@ -98,3 +76,28 @@ __Send any one of the available command, as a reply to a valid link/magnet/torre
 """,
         disable_web_page_preview=True,
     )
+
+'''
+/{RCLONE_COMMAND} : This will change your drive config on fly.(First one will be default)
+ 
+/{CLONE_COMMAND_G}: This command is used to clone gdrive files or folder using gclone.
+Syntax:- `[ID of the file or folder][one space][name of your folder only(If the id is of file, don't put anything)]` and then reply /gclone to it.
+
+/{GLEECH_COMMAND}: This command should be used as reply to a magnetic link, a torrent link, or a direct link. And this will download the files from the given link or torrent and will upload to the cloud using rclone.
+ 
+/{GLEECH_ZIP_COMMAND} This command will compress the folder/file and will upload to your cloud.
+ 
+/{LEECH_UNZIP_COMMAND}: This will unarchive file and upload to telegram.
+ 
+/{GLEECH_UNZIP_COMMAND}: This will unarchive file and upload to cloud.
+ 
+/{TELEGRAM_LEECH_COMMAND}: This will mirror the telegram files to ur respective cloud .
+ 
+/{TELEGRAM_LEECH_UNZIP_COMMAND}: This will unarchive telegram file and upload to cloud.
+ 
+/{GET_SIZE_G}: This will give you total size of your destination folder in cloud.
+
+/{GYTDL_COMMAND}: This will download and upload to your cloud.
+ 
+/{GPYTDL_COMMAND}: This download youtube playlist and upload to your cloud.
+'''
