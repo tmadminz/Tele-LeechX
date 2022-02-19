ea44c82f68ee5ebb64c6784889a9e9271b67f6ff
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-02-20 00:24:37 +0530

    Revert gdtot.nl

--- tobrot/helper_funcs/direct_link_generator.py
+++ tobrot/helper_funcs/direct_link_generator.py
@@ -27,7 +27,7 @@ from lk21.extractors.bypasser import Bypass
 from bs4 import BeautifulSoup
 from base64 import standard_b64encode
 
-from tobrot import UPTOBOX_TOKEN, LOGGER, EMAIL, PWSSD, CRYPT, GDRIVE_FOLDER_ID
+from tobrot import UPTOBOX_TOKEN, LOGGER, EMAIL, PWSSD, CRYPT, PHPSESSID, GDRIVE_FOLDER_ID
 from tobrot.helper_funcs.exceptions import DirectDownloadLinkException
 
 def direct_link_generator(text_url: str):
@@ -472,24 +472,40 @@ def krakenfiles(page_link: str) -> str:
         raise DirectDownloadLinkException(
             f"Failed to acquire download URL from kraken for : {page_link}")
 
+cookies = {"PHPSESSID": PHPSESSID, "crypt": CRYPT}
 
 def gdtot(url: str) -> str:
     """ Gdtot google drive link generator
     By https://github.com/xcscxr """
 
     if CRYPT is None:
-        raise DirectDownloadLinkException("ERROR: CRYPT cookie not provided")
+        raise DirectDownloadLinkException("ERROR: PHPSESSID and CRYPT variables not provided")
 
-    with requests.Session() as client:
-        client.cookies.update({'crypt': CRYPT})
-        res = client.get(url)
-        res = client.get(f"https://new.gdtot.nl/dld?id={url.split('/')[-1]}")
-    matches = re.findall('gd=(.*?)&', res.text)
-    try:
-        decoded_id = b64decode(str(matches[0])).decode('utf-8')
-    except:
-        raise DirectDownloadLinkException("ERROR: Try in your broswer, mostly file not found!")
-    return f'https://drive.google.com/open?id={decoded_id}'
+    headers = {'upgrade-insecure-requests': '1',
+               'save-data': 'on',
+               'user-agent': 'Mozilla/5.0 (Linux; Android 10; Redmi 8A Dual) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36',
+               'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+               'sec-fetch-site': 'same-origin',
+               'sec-fetch-mode': 'navigate',
+               'sec-fetch-dest': 'document',
+               'referer': '',
+               'prefetchAd_3621940': 'true',
+               'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7'}
+
+    r1 = requests.get(url, headers=headers, cookies=cookies).content
+    s1 = BeautifulSoup(r1, 'html.parser').find('button', id="down").get('onclick').split("'")[1]
+    headers['referer'] = url
+    s2 = BeautifulSoup(requests.get(s1, headers=headers, cookies=cookies).content, 'html.parser').find('meta').get('content').split('=',1)[1]
+    headers['referer'] = s1
+    s3 = BeautifulSoup(requests.get(s2, headers=headers, cookies=cookies).content, 'html.parser').find('div', align="center")
+    if s3 is not None:
+        return s3.find(
+            'a', class_="btn btn-outline-light btn-user font-weight-bold"
+        ).get('href')
+
+    s3 = BeautifulSoup(requests.get(s2, headers=headers, cookies=cookies).content, 'html.parser')
+    status = s3.find('h4').text
+    raise DirectDownloadLinkException(f"ERROR: {status}") 
 
 
 def gplink(url: str) -> str:
