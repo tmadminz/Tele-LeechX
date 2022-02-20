b144ee8f2f34386c2de8b4135d8b967935ab7b71
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-02-20 09:02:19 +0530

    Update Ouo for Better ment

--- tobrot/helper_funcs/direct_link_generator.py
+++ tobrot/helper_funcs/direct_link_generator.py
@@ -705,43 +705,47 @@ def ouo(url: str) -> str:
     """ Ouo Bypasser generator
     By https://github.com/xcscxr """
 
-    client = requests.Session()
-    tempurl = url.replace("ouo.press", "ouo.io")
-    p = urlparse(tempurl)
-    id = tempurl.split('/')[-1]
-    res = client.get(tempurl)
-    next_url = f"{p.scheme}://{p.hostname}/go/{id}"
-    url_base = 'https://www.google.com/recaptcha/'
-    post_data = "v={}&reason=q&c={}&k={}&co={}"
-    client = requests.Session()
-    client.headers.update({
-        'content-type': 'application/x-www-form-urlencoded'
-    })
-    matches = re.findall('([api2|enterprise]+)\/anchor\?(.*)', ANCHOR_URL)[0]
-    url_base += matches[0]+'/'
-    params = matches[1]
-    res = client.get(url_base+'anchor', params=params)
-    token = re.findall(r'"recaptcha-token" value="(.*?)"', res.text)[0]
-    params = dict(pair.split('=') for pair in params.split('&'))
-    post_data = post_data.format(params["v"], token, params["k"], params["co"])
-    res = client.post(url_base+'reload', params=f'k={params["k"]}', data=post_data)
-    answer = re.findall(r'"rresp","(.*?)"', res.text)[0]
-    ans = answer
-
-    for _ in range(2):
-        if res.headers.get('Location'):
-            break
-        bs4 = BeautifulSoup(res.content, 'lxml')
-        inputs = bs4.form.findAll("input", {"name": re.compile(r"token$")})
-        data = { input.get('name'): input.get('value') for input in inputs }
-        #ans = RecaptchaV3(ANCHOR_URL)
-        data['x-token'] = ans
-        h = {
+    try:
+        client = requests.Session()
+        tempurl = url.replace("ouo.press", "ouo.io")
+        p = urlparse(tempurl)
+        id = tempurl.split('/')[-1]
+        res = client.get(tempurl)
+        next_url = f"{p.scheme}://{p.hostname}/go/{id}"
+        url_base = 'https://www.google.com/recaptcha/'
+        post_data = "v={}&reason=q&c={}&k={}&co={}"
+        client = requests.Session()
+        client.headers.update({
             'content-type': 'application/x-www-form-urlencoded'
-        }
-        res = client.post(next_url, data=data, headers=h, allow_redirects=False)
-        next_url = f"{p.scheme}://{p.hostname}/xreallcygo/{id}"
+        })
+        matches = re.findall('([api2|enterprise]+)\/anchor\?(.*)', ANCHOR_URL)[0]
+        url_base += matches[0]+'/'
+        params = matches[1]
+        res = client.get(url_base+'anchor', params=params)
+        token = re.findall(r'"recaptcha-token" value="(.*?)"', res.text)[0]
+        params = dict(pair.split('=') for pair in params.split('&'))
+        post_data = post_data.format(params["v"], token, params["k"], params["co"])
+        res = client.post(url_base+'reload', params=f'k={params["k"]}', data=post_data)
+        answer = re.findall(r'"rresp","(.*?)"', res.text)[0]
+        ans = answer
+
+        for _ in range(2):
+            if res.headers.get('Location'):
+                break
+            bs4 = BeautifulSoup(res.content, 'lxml')
+            inputs = bs4.form.findAll("input", {"name": re.compile(r"token$")})
+            data = { input.get('name'): input.get('value') for input in inputs }
+            #ans = RecaptchaV3(ANCHOR_URL)
+            data['x-token'] = ans
+            h = {
+                'content-type': 'application/x-www-form-urlencoded'
+            }
+            res = client.post(next_url, data=data, headers=h, allow_redirects=False)
+            next_url = f"{p.scheme}://{p.hostname}/xreallcygo/{id}"
+    except:
+        return ouo_bypass(url.replace("ouo.io", "ouo.press"))
     return res.headers.get('Location')
+
     '''
     return {
         'original_link': url,
