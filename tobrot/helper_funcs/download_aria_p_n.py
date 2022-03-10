9f3cde6641093901891f112f7810116f9d6eb133
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-03-10 13:23:07 +0530

    Add Mega Error

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -174,7 +174,12 @@ def add_url(aria_instance, text_url, c_file_name):
         return (
             False,
             "⛔ **FAILED** ⛔ \n\n⌧ <i>Please do not send Drive links to Process. I can't Process Those !!</i>",
-        ) 
+        )
+     elif "mega.nz" in text_url or "mega.co.nz" in text_url:
+        return (
+            False,
+            "⛔ **FAILED** ⛔ \n\n⌧ <i>Please do not send Mega links . I am yet not able to Process Those !!</i>",
+        )
     elif is_gdtot_link(text_url) or is_hubdrive_link(text_url):
         return (
             False,
