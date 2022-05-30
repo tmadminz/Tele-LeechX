706feb85f02a82958374edf8cdf915cfa00254d9
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-05-30 21:19:17 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -48,7 +48,7 @@ async def aria_start():
     aria2_daemon_start_cmd = []
     # start the daemon, aria2c command
     aria2_daemon_start_cmd.append("aria2c")
-    aria2_daemon_start_cmd.append("--conf-path=/app/Github_Repo/tobrot/aria2/aria2.conf")
+    aria2_daemon_start_cmd.append("--conf-path=/app/tobrot/aria2/aria2.conf")
     aria2_daemon_start_cmd.append("--allow-overwrite=true")
     aria2_daemon_start_cmd.append("--daemon=true")
     # aria2_daemon_start_cmd.append(f"--dir={DOWNLOAD_LOCATION}")
