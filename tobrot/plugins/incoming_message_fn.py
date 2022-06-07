1e4698a3a7e751fbf695478d2f3a6d3f50069107
DemonSilent <105409614+DemonSilent@users.noreply.github.com>
2022-06-07 23:21:54 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -116,10 +116,11 @@ async def incoming_message_f(client, message):
         text__ += f"🔗 <b>Link</b> : <code>{link}</code>"
         
     link_text = await message.reply_text(text=text__, parse_mode="html", quote=True, disable_web_page_preview=True)
+    LOGGER.info(link_text.link)
     LEECH_LOGS = -1001569981856
     # Send Log Message to Channel 
     if not txtCancel:
-        logs_msg = bot.send_message(chat_id=LEECH_LOGS, text=text__+"\n\n#LeechStart #FXLogs", parse_mode="html", disable_web_page_preview=True)
+        logs_msg = bot.send_message(chat_id=LEECH_LOGS, text=text__+"\nLink : {link_text.link}\n\n#LeechStart #FXLogs", parse_mode="html", disable_web_page_preview=True)
     LOGGER.info(f"Leech Started : {message.from_user.first_name}")
 
     i_m_sefg = await message.reply_text("<code>Processing ... 🔄</code>", quote=True)
