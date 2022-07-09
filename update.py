6faa0fca5726222dbea00857b079b25403617f49
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-07-10 00:07:23 +0530

    Create update.py

---
+++ update.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) 5MysterySD | Anasty17 ( MLTB )
+#
+# Copyright 2022 - TeamTele-LeechX
+# 
+# This is Part of < https://github.com/5MysterySD/Tele-LeechX >
+# All Right Reserved
+
+from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
+from os import path as ospath, environ
+from subprocess import run as srun
+from requests import get as rget
+from dotenv import load_dotenv
+
+if ospath.exists('FuZionXLogs.txt'):
+    with open('FuZionXLogs.txt', 'r+') as f:
+        f.truncate(0)
+
+basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[FileHandler('FuZionXLogs.txt'), StreamHandler()],
+                    level=INFO)
+
+load_dotenv('config.env', override=True)
+
+UPSTREAM_REPO = environ.get('UPSTREAM_REPO', "https://github.com/5MysterySD/Tele-LeechX")
+UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', "master")
+try:
+    if len(UPSTREAM_REPO) == 0:
+       raise TypeError
+except:
+    UPSTREAM_REPO = None
+try:
+    if len(UPSTREAM_BRANCH) == 0:
+       raise TypeError
+except:
+    UPSTREAM_BRANCH = 'master'
+
+if UPSTREAM_REPO is not None:
+    if ospath.exists('.git'):
+        srun(["rm", "-rf", ".git"])
+
+    update = srun([f"git init -q \
+                     && git config --global user.email e.anastayyar@gmail.com \
+                     && git config --global user.name mltb \
+                     && git add . \
+                     && git commit -sm update -q \
+                     && git remote add origin {UPSTREAM_REPO} \
+                     && git fetch origin -q \
+                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+
+    if update.returncode == 0:
+        log_info(f'Successfully updated with latest commit from {UPSTREAM_REPO}')
+    else:
+        log_error(f'Something went wrong while updating, check {UPSTREAM_REPO} if valid or not!')
