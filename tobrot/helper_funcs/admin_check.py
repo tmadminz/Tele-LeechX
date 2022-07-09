d260c7cf0c18be17b8a597886c42c344b7edfe0c
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-07-09 21:34:07 +0530

    Pyrogram.types Update

--- tobrot/helper_funcs/admin_check.py
+++ tobrot/helper_funcs/admin_check.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from tobrot import AUTH_CHANNEL
+# (c) Shrimadhav U K | gautamajay52 | MaxxRider | 5MysterySD | Other Contributors 
+#
+# Copyright 2022 - TeamTele-LeechX
+# 
+# This is Part of < https://github.com/5MysterySD/Tele-LeechX >
+# All Right Reserved
 
+from pyrogram.types import enums
 
 async def AdminCheck(client, chat_id, user_id):
     chat = await client.get_chat(chat_id)
-    if chat.type == "private" and chat_id in AUTH_CHANNEL:
+    if chat.type == enums.ChatType.PRIVATE and chat_id in AUTH_CHANNEL:
         return True
     SELF = await client.get_chat_member(chat_id=chat_id, user_id=user_id)
     admin_strings = ["creator", "administrator"]
     # https://git.colinshark.de/PyroBot/PyroBot/src/branch/master/pyrobot/modules/admin.py#L69
-    if SELF.status not in admin_strings:
+    if SELF.status != enums.ChatMemberStatus.ADMINISTRATOR or SELF.status != enums.ChatMemberStatus.OWNER:
         return False
     else:
         return True
