0bf64d579571b6cdb9bbbcf2d36bc3d3bf7f2902
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-07-11 07:41:57 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -145,6 +145,7 @@ async def upload_to_tg(
                 yt_thumb,
                 prm_atv
             )
+            LOGGER.info(sent_message)
             if sent_message is not None:
                 dict_contatining_uploaded_files[
                     os.path.basename(local_file_name)
